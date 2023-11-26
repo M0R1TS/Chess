b584af03c4bbd67d67d4ae7ebf92faacede070a6
morits <mor1ts@mail.ru>
2023-11-26 22:48:49 +0300

    11

---
+++ src/DZ/ChessPiece.java
@@ -0,0 +1,16 @@
+package DZ;
+
+public abstract class ChessPiece {
+    boolean check = true;
+    String color;
+
+    public ChessPiece(String color) {
+        this.color = color;
+    }
+
+    public abstract String getColor();
+
+    public abstract boolean canMoveToPosition(ChessBoard chessBoard, int line, int column, int toLine, int toColumn);
+
+    public abstract String getSymbol();
+}
